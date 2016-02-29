13a554006422b6fa8cc4b18f4d85c0b5371796a1
Vaibhav Todi <vaibhavtodi1989@gmail.com>
2016-02-29 17:10:15 +0530

    updating the entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,4 @@
 #!/bin/sh
 
-exec 2>&1
-set -e
+exec /usr/share/elasticsearch/bin/elasticsearch --default.path.conf=/etc/elasticsearch
 
-# Exporting the java path
-export JAVA=/usr/lib/jvm/java-8-oracle
-
-# Setting up ElasticSearch Configguartion file
-
-  # Setting the Cluster name
-  sed    -i     's/es_cluster/graylog/'                                /etc/elasticsearch/elasticsearch.yml
-
-  # Setting the path for storing the data
-  sed    -i     's/es_data_path/\/var\/lib\/elasticsearch/'            /etc/elasticsearch/elasticsearch.yml
-
-  # Setting the path for storing the logs
-  sed    -i     's/es_log_path/\/var\/log\/elasticsearch/'             /etc/elasticsearch/elasticsearch.yml
-
-  # Setting the network host - IP
-  sed    -i     's/es_network_host/0\.0\.0\.0/'                        /etc/elasticsearch/elasticsearch.yml
-
-  # Setting the http port
-  sed    -i     's/es_http_port/9200/'                                 /etc/elasticsearch/elasticsearch.yml
-
-  # Setting the unicast
-  sed    -i     's/es_unicast/[\"0\.0\.0\.0:9300\"]/'                  /etc/elasticsearch/elasticsearch.yml
-
-# Starting the elasticsearch
-exec service elasticsearch start
